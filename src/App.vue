3e74b415073ef514333a9b3077d884b69d494a76
enkap24 <117618371+enkap24@users.noreply.github.com>
2022-12-23 15:06:40 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { ref } from "vue";
+
+let msg = "Hello friends";
+let refMsg = ref(msg);
+
+function logChange() {
+  console.log(`msg value: ${msg}`);
+  console.log(`refMsg value: ${refMsg.value}`);
+}
+// component logic
+// declare some reactive state here.
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+  <h1>{{ refMsg }}</h1>
+  <input v-model="msg" /> <br />
+  <input v-model="refMsg" />
+  <!-- first comment this `input v-model="refMsg"` line out and see -->
+  <br />
+  <button @click="logChange">log change</button>
+</template>
